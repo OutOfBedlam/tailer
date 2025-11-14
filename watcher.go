f403ec2646d7f37fa730a9b602dd39990847fef9
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2025-11-14 11:55:13 +0900

    colors

--- watcher.go
+++ watcher.go
@@ -12,6 +12,7 @@ type handler struct {
 	Filename  string
 	CutPrefix string
 	fsServer  http.Handler
+	tailOpts  []Option
 }
 
 func Handler(cutPrefix string, filepath string) http.Handler {
@@ -19,48 +20,33 @@ func Handler(cutPrefix string, filepath string) http.Handler {
 		Filename:  filepath,
 		CutPrefix: cutPrefix,
 		fsServer:  http.FileServerFS(staticFS),
+		tailOpts: []Option{
+			WithPollInterval(500 * time.Millisecond),
+			WithBufferSize(1000),
+		},
 	}
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if strings.HasSuffix(r.URL.Path, "watch.stream") {
-		watch := NewWatcher(string(h.Filename))
-		watch.ServeHTTP(w, r)
-		return
-	}
-
-	r.URL.Path = "static/" + strings.TrimPrefix(r.URL.Path, h.CutPrefix)
-	h.fsServer.ServeHTTP(w, r)
-}
-
-//go:embed static/*
-var staticFS embed.FS
-
-func NewWatcher(filename string) http.Handler {
-	return &Watcher{
-		filename: filename,
+		h.serveWatcher(w, r)
+	} else {
+		h.serveStatic(w, r)
 	}
 }
 
-type Watcher struct {
-	filename string
-	tail     *Tail
-}
-
-var _ http.Handler = (*Watcher)(nil)
-
-func (lw *Watcher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if lw.filename == "" {
-		http.Error(w, "Log filename not configured", http.StatusNotImplemented)
+func (h handler) serveWatcher(w http.ResponseWriter, r *http.Request) {
+	if h.Filename == "" {
+		http.Error(w, "Filename not configured", http.StatusNotImplemented)
 		return
 	}
 
-	lw.tail = New(lw.filename, WithPollInterval(500*time.Millisecond))
-	if err := lw.tail.Start(); err != nil {
-		http.Error(w, "Failed to start log watcher", http.StatusInternalServerError)
+	tail := New(h.Filename, h.tailOpts...)
+	if err := tail.Start(); err != nil {
+		http.Error(w, "Failed to start watcher", http.StatusInternalServerError)
 		return
 	}
-	defer lw.tail.Stop()
+	defer tail.Stop()
 
 	rc := http.NewResponseController(w)
 	w.Header().Set("Content-Type", "text/event-stream")
@@ -69,13 +55,48 @@ func (lw *Watcher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	rc.Flush()
 
+	flushTicker := time.NewTicker(1 * time.Second)
+	defer flushTicker.Stop()
 	for {
 		select {
-		case line := <-lw.tail.Lines():
-			fmt.Fprintf(w, "data: %s\n\n", line)
+		case <-flushTicker.C:
 			rc.Flush()
+		case line := <-tail.Lines():
+			fmt.Fprintf(w, "data: %s\n\n", colors(line))
 		case <-r.Context().Done():
 			return
 		}
 	}
 }
+
+//go:embed static/*
+var staticFS embed.FS
+
+func (h handler) serveStatic(w http.ResponseWriter, r *http.Request) {
+	r.URL.Path = "static/" + strings.TrimPrefix(r.URL.Path, h.CutPrefix)
+	h.fsServer.ServeHTTP(w, r)
+}
+
+// colors formats a line for xterm js coloring
+// For now, it just converts TRACE, DEBUG, INFO, WARN, ERROR to colors
+func colors(line string) string {
+	// Replace log levels with colored versions
+	line = strings.ReplaceAll(line, "TRACE", colorTrace+"TRACE"+colorReset)
+	// line = strings.ReplaceAll(line, "DEBUG", colorDebug+"DEBUG"+colorReset)
+	line = strings.ReplaceAll(line, "INFO", colorInfo+"INFO"+colorReset)
+	line = strings.ReplaceAll(line, "WARN", colorWarn+"WARN"+colorReset)
+	line = strings.ReplaceAll(line, "ERROR", colorError+"ERROR"+colorReset)
+
+	return line
+}
+
+// ANSI color codes for xterm.js
+const (
+	// colorCyan  = "\033[36m" // Cyan
+	// colorGreen  = "\033[32m" // Green
+	colorReset = "\033[0m"
+	colorTrace = "\033[37m" // Light gray
+	colorInfo  = "\033[34m" // Blue
+	colorWarn  = "\033[33m" // Yellow
+	colorError = "\033[31m" // Red
+)
